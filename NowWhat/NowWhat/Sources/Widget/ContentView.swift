004b8984a9ab71fc46a3cab48a5a4f2c9a721b79
Karsten Westra <karsten@gpal.nl>
2023-11-30 10:07:27 +0100

    [WIP] scanning diferrent Github API response objects.

--- NowWhat/NowWhat/Sources/Widget/ContentView.swift
+++ NowWhat/NowWhat/Sources/Widget/ContentView.swift
@@ -9,17 +9,25 @@ import SwiftUI
 
 class API: ObservableObject {
 
-  @Published var json: String = ""
-
   func loadUserProfile() {
     let username = Bundle.main.object(forInfoDictionaryKey: "GITHUB_USERNAME") as? String ?? ""
-    let url = URL(string: "https://api.github.com/users/\(username)")!
 
     Task {
-      let (data, _) = try await URLSession.shared.data(from: url)
-      if let string = String(data: data, encoding: .utf8) {
-        DispatchQueue.main.async { [weak self] in
-          self?.json = string
+      // User Info
+      let url = URL(string: "https://api.github.com/users/\(username)")!
+      let (profileData, _) = try await URLSession.shared.data(from: url)
+      if let profile = String(data: profileData, encoding: .utf8) {
+        DispatchQueue.main.async {
+          print("profile = \(profile)")
+        }
+      }
+
+      // User repos; this should be retrieved from `repos_url` obtained in previous users call.
+      let repoUrl = URL(string: "https://api.github.com/users/\(username)/repos")!
+      let (repoData, _) = try await URLSession.shared.data(from: repoUrl)
+      if let repos = String(data: repoData, encoding: .utf8) {
+        DispatchQueue.main.async {
+          print("repos = \(repos)")
         }
       }
     }
@@ -37,9 +45,7 @@ struct ContentView: View {
         .foregroundColor(.accentColor)
       Text("So, now what!?")
       Text(Bundle.main.object(forInfoDictionaryKey: "GITHUB_USERNAME") as? String ?? "")
-      Text(vm.json)
     }
-    .padding()
     .onAppear {
       vm.loadUserProfile()
     }
